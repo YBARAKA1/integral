c21cf9d12cee22fec05be3ff269fe8d0c3a8d8ba
YBARAKA1 <jeffbaraka1@gmail.com>
2024-05-28 17:36:17 +0300

    still on the about section,

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import { Main } from "next/document";
 import Image from "next/image";
 
@@ -8,6 +10,7 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto
      sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems}/>
         <Hero />
         <Grid />
       </div>
